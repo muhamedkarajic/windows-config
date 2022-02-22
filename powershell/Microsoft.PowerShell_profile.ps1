dd261902e3893515f5d24d2a96e60182e56c2d8f
muhamedkarajic <muhamed.karajic@edu.fit.ba>
2022-02-22 11:38:26 +0100

    adding new scripts icons etc

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
+Clear-Host
 if ($env:TERM_PROGRAM -eq "vscode") {
   Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+}
+
+function prompt {
+  $p = Split-Path -leaf -path (Get-Location)
+  "$p> "
 }
